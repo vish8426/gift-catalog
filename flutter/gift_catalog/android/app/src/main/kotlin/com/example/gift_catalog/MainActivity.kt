e263347444e5ca356acb0f051988d872a7b39a8a
vish8426 <vishantp@live.com.au>
2025-07-31 00:05:57 +1000

    Reverting Project to a Working Baseline

--- flutter/gift_catalog/android/app/src/main/kotlin/com/example/gift_catalog/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gift_catalog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter/gift_catalog/run-flutter-app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-flutter emulators --launch Medium_Phone_API_36.0
-timeout /t 10 /nobreak > NUL
-flutter run
